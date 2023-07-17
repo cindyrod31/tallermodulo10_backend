5005b79d85136af4e0f799f3717e4200afbba379
cindyrod31 <131316757+cindyrod31@users.noreply.github.com>
2023-07-17 01:33:24 -0500

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--CND BOOTSTARP-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/calculadora-style.css">
+
+    <title>Calculadora</title>
+</head>
+<body>
+<a href="index.html#section-respuestas">   
+        <img class="home" src="img/hogar.png" alt="">
+    </a>
+    <div class="main-frame-cal">
+    <form action="" method="POST">
+        <div class="form-group">
+            <label for="exampleFormControlInput1">Número 1:</label>
+            <input type="number" name="n1" class="form-control" id="exampleFormControlInput1" placeholder="Digite Número 1">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput2">Número 2:</label>
+            <input type="number" name="n2" class="form-control" id="exampleFormControlInput2" placeholder="Digite número 2">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Seleccione la operación:</label>
+            <select class="form-control" name="tipo" id="exampleFormControlSelect1">
+                <option value="1">Suma</option>
+                <option value="2">Resta</option>
+                <option value="3">Multiplicación</option>
+                <option value="4">División</option>
+                <option value="5">Raíz Cuadrada</option>
+            </select>
+        </div>
+        <button type="submit" name="operar" class="btn btn-outline-info">Resultado</button>
+    </form>
+
+    <?php
+    $n1 = $n2 = $tipo = $result = '';
+
+    if (isset($_POST['operar'])) {
+        $n1 = $_POST['n1'];
+        $n2 = $_POST['n2'];
+        $tipo = $_POST['tipo'];
+
+        if (empty($n1) && empty($n2) && in_array($tipo, ['1', '2', '3', '4'])) {
+            // Si ambos campos están vacíos y la operación es suma, resta, multiplicación o división
+            echo "<script>alert('Debe ingresar valores en las casillas Número 1 y Número 2 para realizar la operación.');</script>";
+        } elseif (empty($n1) && in_array($tipo, ['1', '2', '3', '4'])) {
+            // Si el campo Número 1 está vacío y la operación es suma, resta, multiplicación o división
+            echo "<script>alert('Debe ingresar un valor en la casilla Número 1 para realizar la operación.');</script>";
+        } elseif (empty($n2) && $tipo !== '5') {
+            // Si el campo Número 2 está vacío y la operación no es la raíz cuadrada
+            echo "<script>alert('Debe ingresar un valor en la casilla Número 2 para realizar la operación.');</script>";
+        } else {
+            switch ($tipo) {
+                case '1':
+                    $result = $n1 + $n2; // Suma
+                    break;
+                case '2':
+                    $result = $n1 - $n2; // Resta
+                    break;
+                case '3':
+                    $result = $n1 * $n2; // Multiplicación
+                    break;
+                case '4':
+                    $result = $n1 / $n2; // División
+                    break;
+                case '5':
+                    if (!empty($n2)) {
+                        // Si se ingresó un valor en el campo Número 2 para la operación de raíz cuadrada
+                        echo "<script>alert('Para calcular la raíz cuadrada, debe ingresar el valor únicamente en la casilla Número 1.');</script>";
+                    } else {
+                        $result = sqrt($n1); // Raíz Cuadrada
+                    }
+                    break;
+            }
+
+            echo "<h1>".$result."</h1>"; // Muestra el resultado
+        }
+    }
+    ?>
+
+</body>
+</html>
